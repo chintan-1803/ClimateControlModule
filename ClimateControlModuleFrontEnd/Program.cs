f88c0548b056dae6379a6591419d170fea406fa1
CHINTAN <CHINTAN@LAPTOP-BK2B12KL>
2025-08-15 18:42:49 +0930

    commit file for change

--- ClimateControlModuleFrontEnd/Program.cs
+++ ClimateControlModuleFrontEnd/Program.cs
@@ -2,28 +2,80 @@
 using Newtonsoft.Json.Linq;
 using System.Text.Json;
 
-Console.WriteLine("Hello, World!");
-
 var apiURL = "http://localhost/WeatherObservation/GetWeatherObservationDataByStationAsyncForLastNHours";
-
-
-var requestObj = new WeatherObservationRequest
-{
-    WmoNumber = 94672
-};
-
-
-using var client = new HttpClient();
-
-var jsonContent = new StringContent(
-    JsonSerializer.Serialize(requestObj),
-    System.Text.Encoding.UTF8,
-    "application/json"
-);
-
 
 try
 {
+
+Dictionary<int, (string Name, int Wmo)> Stations = new Dictionary<int, (string, int)>
+{
+    { 1, ("Adelaide Airport", 94672) },
+    { 2, ("Edinburgh", 95676) },
+    { 3, ("Hindmarsh Island", 94677) },
+    { 4, ("Kuitpo", 94683) }
+};
+
+    const int DefaultChoice = 1;
+
+    Console.WriteLine("Showing default station data:\n");
+
+    await GetWeatherObservationDataByStationAsyncForLastNHours(apiURL, Stations, DefaultChoice);
+
+    bool exit = false;
+
+    while (!exit)
+    {
+        
+        Console.WriteLine("Select a weather observation station:");
+        foreach (var station in Stations)
+        {
+            Console.WriteLine($"{station.Key}. {station.Value.Name}");
+        }
+
+        Console.WriteLine("\n Enter a number between 1 and 4");
+        Console.WriteLine("If you enter an invalid number, the system will display data for Adelaide Airport.\r\nPress 0 to exit.");
+        string input = Console.ReadLine();
+
+        if (input == "0")
+        {
+            exit = true;
+            Console.WriteLine("Exiting...");
+            break;
+        }
+
+        int choice;
+        if (!int.TryParse(input, out choice) || !Stations.ContainsKey(choice))
+        {
+            Console.WriteLine("Invalid choice! Using default station: Adelaide Airport.");
+            choice = DefaultChoice;
+        }
+
+        await GetWeatherObservationDataByStationAsyncForLastNHours(apiURL, Stations, choice);
+
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception: {ex.Message}");
+}
+
+static async Task GetWeatherObservationDataByStationAsyncForLastNHours(string apiURL, Dictionary<int, (string Name, int Wmo)> Stations, int choice)
+{
+    var selectedStation = Stations[choice];
+
+    var requestObj = new WeatherObservationRequest
+    {
+        WmoNumber = selectedStation.Wmo
+    };
+    var client = new HttpClient();
+
+    var jsonContent = new StringContent(
+        JsonSerializer.Serialize(requestObj),
+        System.Text.Encoding.UTF8,
+        "application/json"
+    );
+
+
     var response = await client.PostAsync(apiURL, jsonContent);
 
     if (response.IsSuccessStatusCode)
@@ -36,9 +88,10 @@ try
 
         WeatherObservationResponse jsonWeatherObject = jObject.ToObject<WeatherObservationResponse>() ?? new WeatherObservationResponse();
 
-        if(jsonWeatherObject.Result == WeatherObservationResult.Success)
+        if (jsonWeatherObject.Result == WeatherObservationResult.Success)
         {
-            Console.WriteLine("Weather Data :");
+            double avgTemperature = Math.Round((double) jsonWeatherObject.WeatherData.Sum(x => x.AirTemp) / jsonWeatherObject.WeatherData.Count(),1);
+            Console.WriteLine("Station Name: " + selectedStation.Name + " Wmo Number: " + selectedStation.Wmo.ToString() + " Average Temperature: " + avgTemperature.ToString() + "\n");
         }
         else
         {
@@ -49,10 +102,4 @@ try
     {
         Console.WriteLine("Something went wrong. Please try again.");
     }
-
-
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Exception: {ex.Message}");
 }
