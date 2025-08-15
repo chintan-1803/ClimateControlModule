4db133e4718f8393435d5531bef3640ef8b0175b
CHINTAN <CHINTAN@LAPTOP-BK2B12KL>
2025-08-15 15:42:33 +0930

    committing changes

--- ClimateControlModuleFrontEnd/Program.cs
+++ ClimateControlModuleFrontEnd/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Json;
+using DTO.WeatherObservation;
+
 Console.WriteLine("Hello, World!");
+
+var apiURL = "http://localhost/WeatherObservation/GetWeatherObservationDataByStation";
+
+
+var requestObj = new WeatherObservationRequest
+{
+    WmoNumber = 94672 
+};
+
+
+using var client = new HttpClient();
+
+var jsonContent = new StringContent(
+    JsonSerializer.Serialize(requestObj),
+    System.Text.Encoding.UTF8,
+    "application/json"
+);
+
+
+try
+{
+    var response = await client.PostAsync(apiURL, jsonContent);
+
+    if (response.IsSuccessStatusCode)
+    {
+        var responseJson = await response.Content.ReadAsStringAsync();
+        var weatherResponse = JsonSerializer.Deserialize<WeatherObservationResponse>(responseJson);
+
+        //Console.WriteLine($"Station: {weatherResponse?.StationName}, Temperature: {weatherResponse?.Temperature}");
+    }
+    else
+    {
+        //Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+    }
+
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception: {ex.Message}");
+}
