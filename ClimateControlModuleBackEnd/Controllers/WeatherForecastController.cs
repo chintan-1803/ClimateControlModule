70cb9093f49b6ee5671d5badcc209abc794bd6f8
CHINTAN <CHINTAN@LAPTOP-BK2B12KL>
2025-08-15 15:16:52 +0930

    commit file for change

--- ClimateControlModuleBackEnd/Controllers/WeatherForecastController.cs
+++ ClimateControlModuleBackEnd/Controllers/WeatherForecastController.cs
@@ -16,7 +16,7 @@ namespace ClimateControlModuleBackEnd.Controllers
         }
 
         // This endpoint retrieves weather observation data by WMO number -- Just take post method for adding more parameters in future
-        [HttpPost(Name = "GetWeatherObservationDataByStation")]
+        [HttpPost("GetWeatherObservationDataByStation")]
         public async Task<WeatherObservationResponse> GetWeatherObservationDataByStationAsync(WeatherObservationRequest objRequest)
         {
             var response =  await _manager.GetWeatherObservationDataByStationAsync(objRequest);
