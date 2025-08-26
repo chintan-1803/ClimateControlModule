337e0748a6d29cd8fd17c4d692766eaf7e736184
CHINTAN <CHINTAN@LAPTOP-BK2B12KL>
2025-08-26 15:20:31 +0930

    commit file for change

---
+++ ClimateControlModuleTests/Controllers/WeatherObservationControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+using Moq;
+using Contracts.Managers;
+using ClimateControlModuleBackEnd.Controllers;
+namespace ClimateControlModuleTests.Controllers
+{
+    public class WeatherObservationControllerTests
+    {
+        private readonly Mock<IWeatherObservationManager> _mockManager;
+        private readonly WeatherObservationController _controller;
+
+        public WeatherObservationControllerTests()
+        {
+            _mockManager = new Mock<IWeatherObservationManager>();
+            _controller = new WeatherObservationController(_mockManager.Object);
+        }
+
+
+        [Fact]
+        public async Task GetWeatherObservationDataByStationAsyncForLastNHours_ReturnsExpectedResponse()
+        {
+            // Arrange
+            var request = new DTO.WeatherObservation.WeatherObservationRequest
+            {
+               WmoNumber = 94672
+            };
+            var expectedResponse = new DTO.WeatherObservation.WeatherObservationResponse
+            {
+                Result = DTO.WeatherObservation.WeatherObservationResult.Success,   
+            };
+
+            _mockManager
+                .Setup(m => m.GetWeatherObservationDataByStationAsyncForLastNHours(request))
+                .ReturnsAsync(expectedResponse);
+            // Act
+            var result = await _controller.GetWeatherObservationDataByStationAsyncForLastNHours(request);
+            // Assert
+            Assert.Equal(expectedResponse, result);
+
+           // _mockManager.Verify(m => m.GetWeatherObservationDataByStationAsyncForLastNHours(request), Times.Once);
+        }
+    }
+}
